49ccb249dd2b15cad5c50f0fc720bcbe45b40c9c
Nick <nickversluis446@gmail.com>
2021-12-03 15:00:37 +0100

    Day 2 done

---
+++ Day-2/src/main/java/me/imspooks/adventofcode/day2/DiveA.java
@@ -0,0 +1,59 @@
+package me.imspooks.adventofcode.day2;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Nick on 03 dec. 2021.
+ * Copyright © ImSpooks
+ */
+public class DiveA {
+
+    public static void main(String[] args) {
+        // Get our file as an input stream
+        final InputStream inputStream = DiveA.class.getResourceAsStream("/input.txt");
+
+        // Create a new reader and make sure our input stream is not null
+        assert inputStream != null;
+        final Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+        try (final BufferedReader bufferedReader = new BufferedReader(reader)) {
+
+            // Keep track of our position
+            final AtomicInteger horizontal = new AtomicInteger(0);
+            final AtomicInteger depth = new AtomicInteger(0);
+
+            // Loop through each line
+            bufferedReader.lines().forEach(line -> {
+                final String[] split = line.split(" ");
+                final int value = Integer.parseInt(split[1]);
+
+                // Update our position
+                switch (split[0].toLowerCase(Locale.ROOT)) {
+                    case "forward": {
+                        horizontal.set(horizontal.get() + value);
+                        break;
+                    }
+
+                    case "up": {
+                        depth.set(depth.get() - value);
+                        break;
+                    }
+
+                    case "down": {
+                        depth.set(depth.get() + value);
+                        break;
+                    }
+                }
+            });
+
+            System.out.println("horizontal = " + horizontal.get());
+            System.out.println("depth = " + depth.get());
+            System.out.println("result = " + (horizontal.get() * depth.get()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
